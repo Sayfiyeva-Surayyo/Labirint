8b3c9851c0fb077062565992c3da089559e87cc1
SayfiyevaSurayyo <sarvarjonsayfiyev@gmail.com>
2024-12-06 21:28:41 +0500

    add soldier function

--- main.py
+++ main.py
@@ -6,4 +6,27 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+# Labirintni yaratish
+def generate_maze(width, height):
+    maze = [[1] * width for _ in range(height)]
+    stack = [(1, 1)]
+    
+    while stack:
+        x, y = stack[-1]
+        maze[y][x] = 0
+        
+        neighbors = []
+        if x > 1 and maze[y][x - 2] == 1: neighbors.append((x - 2, y))
+        if x < width - 2 and maze[y][x + 2] == 1: neighbors.append((x + 2, y))
+        if y > 1 and maze[y - 2][x] == 1: neighbors.append((x, y - 2))
+        if y < height - 2 and maze[y + 2][x] == 1: neighbors.append((x, y + 2))
+        
+        if neighbors:
+            nx, ny = random.choice(neighbors)
+            maze[(y + ny) // 2][(x + nx) // 2] = 0
+            stack.append((nx, ny))
+        else:
+            stack.pop()
+    
+    return maze
 
